724fce9b1dcdcc83bca4e0ed150d14382fa693ba
Petter Holmström <petter@vaadin.com>
2023-09-30 11:34:30 +0300

    Add MarketPriceComponent

---
+++ src/main/java/org/example/components/marketprice/MarketPriceComponent.java
@@ -0,0 +1,37 @@
+package org.example.components.marketprice;
+
+import org.example.components.orderprocessing.domain.primitives.PositiveMoney;
+import org.example.components.orderprocessing.domain.primitives.Stock;
+import org.example.components.orderprocessing.messages.TransactionCreatedEvent;
+import org.example.pipesandfilters.HasInput;
+import org.example.pipesandfilters.MessageSink;
+import org.example.stereotype.Component;
+import org.example.stereotype.ThreadSafe;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+@ThreadSafe
+public final class MarketPriceComponent implements HasInput<TransactionCreatedEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(MarketPriceComponent.class);
+    private final ConcurrentMap<Stock, PositiveMoney> currentMarketPrice = new ConcurrentHashMap<>();
+
+    private void onTransactionCreatedEvent(@NotNull TransactionCreatedEvent event) {
+        currentMarketPrice.put(event.transaction().stock(), event.transaction().pricePerShare());
+        logger.info("Current price of {} is {}", event.transaction().stock(), event.transaction().pricePerShare());
+    }
+
+    public @NotNull Optional<PositiveMoney> getMarketPrice(@NotNull Stock stock) {
+        return Optional.ofNullable(currentMarketPrice.get(stock));
+    }
+
+    @Override
+    public @NotNull MessageSink<TransactionCreatedEvent> input() {
+        return this::onTransactionCreatedEvent;
+    }
+}
