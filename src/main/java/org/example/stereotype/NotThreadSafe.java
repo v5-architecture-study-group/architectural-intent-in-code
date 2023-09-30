c83aab845d3f31c0928062d8e23459c00d28e057
Petter Holmström <petter@vaadin.com>
2023-09-30 15:08:24 +0300

    Add @Documented to stereotype annotations

--- src/main/java/org/example/stereotype/NotThreadSafe.java
+++ src/main/java/org/example/stereotype/NotThreadSafe.java
@@ -1,8 +1,10 @@
 package org.example.stereotype;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
+@Documented
 public @interface NotThreadSafe {
 }
