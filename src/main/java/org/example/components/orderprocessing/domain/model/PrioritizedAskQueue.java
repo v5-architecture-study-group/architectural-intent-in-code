aed8cc6e04fee346c16df607368ab62cb325fc0f
Petter Holmström <petter@vaadin.com>
2023-09-30 15:53:01 +0300

    Add queue interfaces

---
+++ src/main/java/org/example/components/orderprocessing/domain/model/PrioritizedAskQueue.java
@@ -0,0 +1,14 @@
+package org.example.components.orderprocessing.domain.model;
+
+import org.example.components.orderprocessing.domain.primitives.OrderId;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public interface PrioritizedAskQueue {
+    @NotNull Optional<Ask> getBestAsk(@NotNull Bid bid);
+
+    void push(@NotNull Ask ask);
+
+    void remove(@NotNull OrderId orderId);
+}
---
+++ src/main/java/org/example/components/orderprocessing/domain/model/PrioritizedBidQueue.java
@@ -0,0 +1,14 @@
+package org.example.components.orderprocessing.domain.model;
+
+import org.example.components.orderprocessing.domain.primitives.OrderId;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public interface PrioritizedBidQueue {
+    @NotNull Optional<Bid> getNextBid();
+
+    void push(@NotNull Bid bid);
+
+    void remove(@NotNull OrderId orderId);
+}
